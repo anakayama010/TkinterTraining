78820a7cf1513d7f14c0f0eefd9db2d2d4cbd827
anakayama010 <121213017+anakayama010@users.noreply.github.com>
2023-03-05 10:02:32 +0900

    ラベルの作り方を修正

--- create/rabel.py
+++ create/rabel.py
@@ -9,9 +9,21 @@ root.title('Rabelの作成')
 
 
 # ラベル作成
-lbl = tkinter.Label(text='Rabel ラベル')
-lbl.place(x=30, y=70)
+lbl1 = tkinter.Label(text='Rabel ラベル')
 
+
+# 文字色・・・foreground、fg
+# 背景色・・・background、bg
+
+# ラベルの文字色
+lbl2 = tkinter.Label(text="foreground='#faf0e6', background='#778899'", foreground='#faf0e6', background='#778899')
+
+# ラベルの背景色（文字色名）
+lbl3 = tkinter.Label(text="foreground='pink', background='blue'", foreground='pink', background='blue') 
+
+lbl1.place(x=30, y=50)
+lbl2.place(x=30, y=80)
+lbl3.place(x=30, y=110)
 
 # 表示
 root.mainloop()
