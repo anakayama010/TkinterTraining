b5c5ba88834d7e91319aef55ed48c64551705050
anakayama010 <121213017+anakayama010@users.noreply.github.com>
2023-03-05 10:50:22 +0900

    ラジオボタンの作り方を加筆修正

--- create/radiobutton.py
+++ create/radiobutton.py
@@ -1,11 +1,57 @@
 import tkinter, tkinter.messagebox
 
 # Tkクラス生成
+root = tkinter.Tk()
+# 画面サイズ
+root.geometry('300x200')
+# 画面タイトル
+root.title('ラジオボタン簡易作成')
+
+# ラジオボタン簡易作成
+rdo1 = tkinter.Radiobutton(root, text='Apple')
+rdo1.place(x=70, y=40)
+
+rdo2 = tkinter.Radiobutton(root, text='Amazon')
+rdo2.place(x=70, y=70)
+
+rdo3 = tkinter.Radiobutton(root, text='Google')
+rdo3.place(x=70, y=100)
+
+root.mainloop()
+
+
+# Tkクラス生成
+root2 = tkinter.Tk()
+# 画面サイズ
+root2.geometry('300x200')
+# 画面タイトル
+root2.title('ラジオボタン基本編')
+
+# チェック有無変数
+var = tkinter.IntVar()
+# value=2のラジオボタンにチェックを入れる
+var.set(2)
+
+# ラジオボタン基本作成
+rdo1 = tkinter.Radiobutton(root2, value=0, variable=var, text='Apple')
+rdo1.place(x=70, y=40)
+
+rdo2 = tkinter.Radiobutton(root2, value=1, variable=var, text='Amazon')
+rdo2.place(x=70, y=70)
+
+rdo3 = tkinter.Radiobutton(root2, value=2, variable=var, text='Google')
+rdo3.place(x=70, y=100)
+
+root2.mainloop()
+
+
+
+# Tkクラス生成
 tki = tkinter.Tk()
 # 画面サイズ
 tki.geometry('300x200')
 # 画面タイトル
-tki.title('ラジオボタン')
+tki.title('ラジオボタン応用編')
 
 
 # ラジオボタンのラベルをリスト化する
